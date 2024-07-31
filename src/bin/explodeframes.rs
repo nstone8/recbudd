a5e03f5a936122b702bd78b958d1bda9bddcfdd4
Nick Stone <nicholasestone@proton.me>
2024-07-31 13:03:59 -0500

    finished first draft

---
+++ src/bin/explodeframes.rs
@@ -0,0 +1,40 @@
+use ciborium;
+use clap::Parser;
+use recbudd;
+use std::fs::{DirBuilder, File};
+use std::io::BufReader;
+use std::path::PathBuf;
+//ask where to save our images
+#[derive(Parser)]
+struct ExplodeArgs {
+    file_path: PathBuf,
+}
+
+fn main() {
+    let path = ExplodeArgs::parse().file_path;
+    let f = File::open(&path).expect("couldn't open file");
+    let mut reader = BufReader::new(f);
+    let mut dir = PathBuf::new();
+    dir.push(path.parent().unwrap());
+    dir.push(format!(
+        "{}_images",
+        path.file_name().unwrap().to_str().unwrap()
+    ));
+    let b = DirBuilder::new();
+    b.create(&dir).expect("couldn't create image directory");
+    //pull off frames until there are none left, save them in a new folder next to the data file
+    loop {
+        match ciborium::from_reader::<recbudd::RecFrame, &mut BufReader<File>>(&mut reader) {
+            Ok(rec_frame) => {
+                let ts = rec_frame.get_timestamp();
+                let im = rec_frame.to_image();
+                let mut im_path = dir.clone();
+                im_path.push(format!("{}.png", ts));
+                im.save(im_path).expect("couldn't save image");
+            }
+            Err(_) => {
+                break;
+            }
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,70 +1,94 @@
-use serde;
-use serde::{Serialize,Deserialize};
-use papillae::Analysis;
-use papillae::{ralston,futures,iced};
-use ralston::image;
-use std::fs::File;
-use core::time::Duration;
-use iced::widget::{Container,text};
-use iced::Theme;
-use papillae::UiMessage;
 use chrono::offset::Local;
-use chrono::{Datelike,Timelike};
-use std::io::BufWriter;
+use chrono::{Datelike, Timelike};
 use ciborium;
+use core::time::Duration;
+use iced::widget::{text, Container};
+use iced::Theme;
+use papillae::Analysis;
+use papillae::UiMessage;
+use papillae::{futures, iced, ralston};
+use ralston::image;
+use serde;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::BufWriter;
 //we need to wrap frame in a newtype so we can derive Serialize on it
-#[derive(Serialize,Deserialize)]
-struct RecFrame {
-    timestamp:Duration,
+#[derive(Serialize, Deserialize)]
+pub struct RecFrame {
+    timestamp: Duration,
     width: u32,
     height: u32,
     data: Vec<u16>,
 }
 
-impl RecFrame{
-    fn new(im:image::DynamicImage,ts:Duration) -> Self {
-	//convert to luma16 (this is what the camera captures)
-	let luma = im.into_luma16();
-	RecFrame{
-	    timestamp:ts,
-	    width: luma.width(),
-	    height: luma.height(),
-	    data: luma.into_vec()
-	}
+impl RecFrame {
+    pub fn new(im: image::DynamicImage, ts: Duration) -> Self {
+        //convert to luma16 (this is what the camera captures)
+        let luma = im.into_luma16();
+        RecFrame {
+            timestamp: ts,
+            width: luma.width(),
+            height: luma.height(),
+            data: luma.into_vec(),
+        }
     }
-    fn to_image(self) -> image::DynamicImage{
-	let luma = image::ImageBuffer::<image::Luma<u16>,Vec<u16>>::from_vec(self.width,self.height,self.data).expect("couldn't build image from saved data");
-	image::DynamicImage::ImageLuma16(luma)
+    pub fn to_image(self) -> image::DynamicImage {
+        let luma = image::ImageBuffer::<image::Luma<u16>, Vec<u16>>::from_vec(
+            self.width,
+            self.height,
+            self.data,
+        )
+        .expect("couldn't build image from saved data");
+        image::DynamicImage::ImageLuma16(luma)
+    }
+    pub fn get_timestamp(&self) -> f32 {
+        self.timestamp.as_secs_f32()
     }
 }
-pub struct RecBudd{
-    file_writer: BufWriter<File>
+pub struct RecBudd {
+    file_writer: BufWriter<File>,
 }
 
 impl RecBudd {
     ///build a new RecBudd which will store images at `filepath`
-    pub fn new_from_path(filepath:&str) -> Self{
-	//create a new file
-	let file = File::create(filepath).expect("couldn't open file");
-	RecBudd{file_writer:BufWriter::new(file)}
+    pub fn new_from_path(filepath: &str) -> Self {
+        //create a new file
+        let file = File::create(filepath).expect("couldn't open file");
+        RecBudd {
+            file_writer: BufWriter::new(file),
+        }
     }
     pub fn new() -> Self {
-	let timestamp = Local::now();
-	RecBudd::new_from_path(&format!("{}_{}_{}_{}_{}.recbudd",timestamp.year(),timestamp.month(),timestamp.day(),timestamp.hour(),timestamp.minute()))
+        let timestamp = Local::now();
+        RecBudd::new_from_path(&format!(
+            "{}_{}_{}_{}_{}.recbudd",
+            timestamp.year(),
+            timestamp.month(),
+            timestamp.day(),
+            timestamp.hour(),
+            timestamp.minute()
+        ))
     }
 }
-    
+
 impl Analysis for RecBudd {
     type DisplayData = Duration;
     fn get_title() -> String {
-	String::from("recbudd")
+        String::from("recbudd")
     }
-    fn process_frame(&mut self, frame:ralston::Frame, mut sender: futures::channel::mpsc::Sender<(image::DynamicImage,Self::DisplayData)>) {
-	//serialize the frame and send it to the display
-	let ts = frame.get_timestamp();
-	let im = frame.to_image();
-	sender.try_send((im.clone(),ts.clone())).expect("couldn't display frame");
-	ciborium::into_writer(&RecFrame::new(im,ts),&mut self.file_writer).expect("serialization error");
+    fn process_frame(
+        &mut self,
+        frame: ralston::Frame,
+        mut sender: futures::channel::mpsc::Sender<(image::DynamicImage, Self::DisplayData)>,
+    ) {
+        //serialize the frame and send it to the display
+        let ts = frame.get_timestamp();
+        let im = frame.to_image();
+        sender
+            .try_send((im.clone(), ts.clone()))
+            .expect("couldn't display frame");
+        ciborium::into_writer(&RecFrame::new(im, ts), &mut self.file_writer)
+            .expect("serialization error");
     }
     fn display_results(
         d: &Self::DisplayData,
